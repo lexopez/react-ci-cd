76ac622c0dbc29f29b2b45e68e1a1236135a6d18
alexander lopez <iamlexlopez@gmail.com>
2026-01-08 14:29:31 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,6 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  // return (
-  //   <>
-  //     <h1>Vite + React</h1>
-  //     <div className="card">
-  //       <button onClick={() => setCount((count) => count + 1)}>
-  //         count is {count}
-  //       </button>
-  //     </div>
-  //   </>
-  // );
   return (
     <div>
       <p>Count: {count}</p>
